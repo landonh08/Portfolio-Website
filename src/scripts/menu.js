48f84dccc2fb97c24a8b53b9c1ec665f8e1abd37
landonh <landonh08@users.noreply.github.com>
2026-01-01 20:49:28 -0500

    wiped most of the content finally going to start building the actuall site

--- src/scripts/menu.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector(".menu");
-console.log("loaded");
-
-menu.addEventListener('click', () => {
-    console.log("click");
-    const isExpanded = menu.getAttribute('aria-expanded') === 'true';
-    menu.setAttribute('aria-expanded', `${!isExpanded}`);
-});
